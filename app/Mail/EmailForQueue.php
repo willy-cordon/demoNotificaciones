2b96dea64cbe4f9ef782205e4ac73a679f42efea
Willy <cordonwilly24@gmail.com>
2022-01-28 11:28:59 -0300

    queue

---
+++ app/Mail/EmailForQueue.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EmailForQueue extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $description;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
