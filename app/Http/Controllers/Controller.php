6cb43136d5de9f08e8b3e4d293ed813b2ea5e42a
Willy <cordonwilly24@gmail.com>
2022-01-28 12:51:09 -0300

    jobs2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Jobs\SendEmail;
 use Illuminate\Http\Request;
+use Illuminate\Queue\Queue;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
@@ -15,19 +18,26 @@ class Controller extends BaseController
      * @return Response
      */
     public function EnvioMail(Request $request){
-        
+
         try {
             $content=$request->input('content');
             $sendto=$request->input('sendto');
            // $content='<h1>prueba enviando el contenido como html desde controller</h1>';
+            /**
+             * * Cola de trabajos
+             */
             Mail::to($sendto)->send(new TestMail($content));
-           
+//            SendEmail::dispatch($request);
+//            Queue::push(new SendEmail($request));
+//            SendEmail::
+//            dispatch(new SendEmail($content,$sendto));
+
             return response()->json(['message'=>'ok'], 200);
         } catch (\Throwable $th) {
             //throw $th;
             return $th;
         }
-       
+
     }
 
 }
--- app/Jobs/SendEmail.php
+++ app/Jobs/SendEmail.php
@@ -2,16 +2,32 @@
 
 namespace App\Jobs;
 
-class SendEmail extends Job
+use App\Mail\TestMail;
+use Illuminate\Bus\Dispatcher;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+
+class SendEmail implements ShouldQueue
 {
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $content;
+    protected $sendTo;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($content,$sendTo)
     {
         //
+        $this->content = $content;
+        $this->sendTo = $sendTo;
+
     }
 
     /**
@@ -22,5 +38,9 @@ class SendEmail extends Job
     public function handle()
     {
         //
+        $email = new TestMail($this->content);
+        Mail::to($this->sendTo)->send($email);
     }
+
+
 }
